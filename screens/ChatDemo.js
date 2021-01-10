27b473661dcecd460927002d6cee2debadd3c11f
NumanArshad <afshanarsha2783@gmail.com>
2021-01-11 02:39:45 +0500

    one to one chat basic demo

--- screens/ChatDemo.js
+++ screens/ChatDemo.js
@@ -1,45 +1,88 @@
-import React, { useState, useCallback, useEffect } from 'react'
-import { GiftedChat } from 'react-native-gifted-chat'
- 
+import React, { useState, useCallback, useEffect } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import firebase from "../utils/firebaseConfig";
+
 export function ChatDemo() {
-  const [messages, setMessages] = useState([]);
- 
+  const [messages, setMessages] = useState([
+    // {
+    //   _id: 1,
+    //   text: "It's going well! How about you?",
+    //   user: {
+    //     _id: 3,
+    //     name: "Name",
+    //     avatar: "https://placeimg.com/140/140/any",
+    //   },
+    // },
+    // {
+    //   _id: 2,
+    //   text: "What do you do for a living?",
+    // },
+    // {
+    //   _id: 3,
+    //   text: "Hello, My name's Hieu Le.\n" + "Nice to meet you!",
+    //   user: {
+    //     _id: 1,
+    //     name: "Name",
+    //     avatar: "https://placeimg.com/140/140/any",
+    //   },
+    // },
+  ]);
+  const messagesRef = firebase
+    .firestore()
+    .collection("chatRooms")
+    .doc("kQjycgqL1HTJ49KpPRub")
+    .collection("messages");
+
+  //console.log("lst is ", messages)
+
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hell developer',
-        name: 'hgh',
-        createdAt: new Date(),
-        
-       avatar: 'https://placeimg.com/140/140/any',  
-        user: {
-          _id: 2,
-          text: 'Hell develo',
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-  
-        },
-       // image: 'https://placeimg.com/140/140/any',
-    
-      },
-    ])
-  }, [])
- 
+    let updateMessages = [];
+    messagesRef.onSnapshot((snapshot) => {
+      console.log("dt is", snapshot.size);
+      snapshot.docChanges().forEach((changes) => {
+//        console.log(changes.doc.data(), changes.doc.id);
+        // const { sentBy };
+        if (changes.type === "added") {
+          const { sentBy, sentAt, text, _id } = changes.doc.data();
+          updateMessages.unshift({
+            _id,
+            createdAt: sentAt,
+            text,
+            user: {
+              _id: sentBy,
+              name: "Name",
+              avatar: "https://placeimg.com/140/140/any",
+            },
+          });
+        }
+      });
+      setMessages(updateMessages);
+    });
+  }, []);
+
   const onSend = useCallback((messages = []) => {
-    console.log("meeesag is ", messages)
-    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-  }, [])
- 
+    const { user, ...messageInfo } = messages[0];
+    //  console.log("meeesag is ", messageInfo);
+    const { _id, text, createdAt } = messageInfo;
+    messagesRef.add({
+      _id,
+      text,
+      sentAt: Date.now(),
+      sentBy: 2,
+    });
+    // setMessages((previousMessages) =>
+    //   GiftedChat.append(previousMessages, messages)
+    // );
+  }, []);
+
   return (
     <GiftedChat
       messages={messages}
-      onSend={messages => onSend(messages)}
+      onSend={(messages) => onSend(messages)}
       user={{
-      _id: 2,
-       
-    }}
-    showUserAvatar
+        _id: 1,
+      }}
+      showUserAvatar
     />
-  )
+  );
 }
