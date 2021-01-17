e9c5053f196f1f50512689df7b42ec2866839ccc
NumanArshad <afshanarsha2783@gmail.com>
2021-01-17 23:16:51 +0500

    expo install for apk

--- screens/About.js
+++ screens/About.js
@@ -155,7 +155,7 @@ alert(JSON.stringify(url))
       <Button title="Add Firetore" onPress={handleAddFirestore} />
       <Button title="Pick image" onPress={handleImagePicker} />
       <Button title="Upload image" onPress={uploadImage} />
-  
+     <Text>happy continious</Text>
       <Button
         title="hey pull request and apk setup"
         onPress={() => navigation.navigate("home")}
